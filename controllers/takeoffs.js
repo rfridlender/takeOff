c95b835de9c44604607c57e46dd41bfb67da216b
Robert Fridlender <robertfridlender@gmail.com>
2023-01-16 19:41:58 -0500

    edit takeoff form to create lock type inputs programatically

--- controllers/takeoffs.js
+++ controllers/takeoffs.js
@@ -24,7 +24,9 @@ function newTakeoff(req, res) {
       res.render('takeoffs/new', {
         title: 'Create Takeoff',
         builders,
-        locks
+        locks,
+        possibleFinishes: ['US3', 'US5', 'US10B', 'US11P', 'US15', 'US15A', 'US19', 'US26', 'US26D', 'US32D'],
+        possibleLockTypes: ['Handleset', 'Interior Trim', 'Entry', 'Deadbolt', 'Passage', 'Privacy', 'Dummy', 'Pocket Passage', 'Pocket Privacy', 'Jumbo Springs', 'Door Saver', 'Door Saver II'],
       })
     })
     .catch(err => {
